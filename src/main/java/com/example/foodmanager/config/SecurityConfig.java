38699a1567d6d6e6b75d6f0ffa061cb8f904f151
Haku_Kobayashi <c1407219@st.kanazawa-it.ac.jp>
2025-10-28 11:04:03 +0900

    アイコンを追加

--- src/main/java/com/example/foodmanager/config/SecurityConfig.java
+++ src/main/java/com/example/foodmanager/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/register", "/css/**", "/js/**", "/webjars/**", "/h2-console/**").permitAll()
+                .requestMatchers("/register", "/css/**", "/js/**", "/webjars/**", "/h2-console/**", "/favicon.ico", "/favicon.svg", "/favicon-16x16.svg").permitAll()
                 .anyRequest().authenticated()
             )
             .formLogin(form -> form
