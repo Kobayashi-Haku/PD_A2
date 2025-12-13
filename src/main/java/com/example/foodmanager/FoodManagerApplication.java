d9b7c8a3dad925b8feb0c4f2f7700360ada73dab
Haku_Kobayashi <c1407219@st.kanazawa-it.ac.jp>
2025-12-13 17:27:28 +0900

    error naosita

--- src/main/java/com/example/foodmanager/FoodManagerApplication.java
+++ src/main/java/com/example/foodmanager/FoodManagerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableAsync // ▼ 追加: これで裏側での処理が可能になります
+@EnableAsync
 public class FoodManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(FoodManagerApplication.class, args);
